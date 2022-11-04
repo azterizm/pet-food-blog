c445b68156dec90f41df6ba6e04fc710b94d95a5
azterizm <abdielprime@gmail.com>
2022-11-04 10:34:25 +0500

    stable create post

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, HashRouter } from 'react-router-dom'
 import App from './App'
 import './css/global.css'
 import 'uno.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
 )
